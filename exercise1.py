1029cee2b5097b53482d54b379ad91cc2202bf08
allan chang <bohanzhang@my.unt.edu>
2020-02-27 22:14:02 +0000

    first commit

--- exercise1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-first_name = 'allan'
-last_name = 'chang'
-age = 20
-birth_day = "00-00-1000"
-
-print("my name is %s %s" % (first_name,last_name))
-print("I was born on %s, and I'm %d years old." % (birth_day,age))
