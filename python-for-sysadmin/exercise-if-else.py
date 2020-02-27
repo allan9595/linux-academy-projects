8b5be07ce7b6865286a308b5dca3e9117f2f05fe
allan chang <bohanzhang@my.unt.edu>
2020-02-27 22:09:10 +0000

    first commit

---
+++ python-for-sysadmin/exercise-if-else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.6
+user = {'admin': True, 'active': True, 'name':'allan'}
+
+if user['admin'] and not user['active']:
+    print(f"(ADMIN) {user['name']}")
+elif user['active'] and not user['admin']:
+    print(f"ACTIVE {user['name']}")
+elif user['admin'] and user['active']:
+    print(f"ACTIVE - (ADMIN) {user['name']}")
+else:
+    print(user['name'])
+
